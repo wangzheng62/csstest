d9d976e3db4e0d1b8f31f056d08b4320a2447bad
wangzheng62 <363579018@qq.com>
2017-06-18 21:50:56 +0800

    584566

---
+++ views.py
@@ -0,0 +1,22 @@
+﻿#！/usr/bin/env python3
+#-*-coding -utf8 -*-
+'router register'
+from flask import Flask,render_template,session,request
+import os
+#变量初始化
+app=Flask(__name__)
+app.secret_key=os.urandom(24)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+@app.route('/echarts')
+def echarts():
+	return render_template('图表绘制.html')
+@app.route('/floatbox')
+def floatbox():
+	return render_template('浮动登录框.html')
+
+if __name__=='__main__':
+	app.debug=True
+	app.run(host='0.0.0.0',port=10000,threaded=True)
